95402a458f962794a6d91b23dca8e31368739264
Youness Lamraoui <ylamraou@e1r8p4.1337.ma>
2022-05-22 01:27:01 +0100

    hi sba7 lkhir darbu 3lik alf dura  nta baqi f nos tiran

--- pipex/GNxtL/main.c
+++ pipex/GNxtL/main.c
@@ -6,31 +6,76 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
+// #include "../libft.h"
+
+
+// int	ft_strcmp(const char *s1, const char *s2)
+// {
+// 	unsigned char	*c1;
+// 	unsigned char	*c2;
+
+// 	c1 = (unsigned char *)s1;
+// 	c2 = (unsigned char *)s2;
+// 	while ((*c1 || *c2))
+// 	{
+// 		if (*c1 != *c2)
+// 			return (*c1 - *c2);
+// 		c1++;
+// 		c2++;
+// 	}
+// 	return (0);
+// }
+
+int	ft_strcmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	*c1;
+	unsigned char	*c2;
+
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
+	while ((*c1 || *c2) && n--)
+	{
+		if (*c1 != *c2)
+			return (*c1 - *c2);
+		c1++;
+		c2++;
+	}
+	return (0);
+}
+
 char	*joinheredoc(char **av)
 {
-	int		i;
+	int		i = 1;
+	int h = strlen(av[1]) + 2 ;
 	char	*hd_content;
 	char	*limiter;
 	char	*tmp;
-
-	limiter = NULL;
+	char	*avlim;
+	avlim = 
+	printf("@%s@\n", av[1]);
 	hd_content = NULL;
 	while (1)
 	{
-		exit(1);
-		limiter = get_next_line(0);
-		if (!strcmp(av[2], limiter))
+		// exit(1);
+		if (i != 0)
 		{
+			limiter = get_next_line(0);
+			printf("contnt = |%s| .. LIMITER = #%s# .. av[1] = [%s]\n", hd_content, limiter, av[1]);
+			i = ft_strcmp(av[1], limiter, (strlen(av[1])) + 1);
+			printf("i = %i\n", i);
 			tmp = hd_content;
 			hd_content = ft_strjoin(hd_content, limiter);
-			free(tmp);
-			free(limiter);
+			// if(tmp)
+			// 	free(tmp);
 		}
 		else
 		{
-			free(limiter);
+			// free(limiter);
 			break;
 		}
+		
+		// if (limiter)
+		// 	free(limiter);
 	}
 	return (hd_content);
 	// hd_content = get
@@ -39,6 +84,7 @@ char	*joinheredoc(char **av)
 	// 	//join (hd_content,'\n');
 	// }
 }
+
 int main(int ac, char **av)
 {
 	int a = 0;
